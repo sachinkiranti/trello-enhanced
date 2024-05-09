796e986ded14c3f0bf163fcb46da5180a7472049
sachinkiranti <sachinkiranti@gmail.com>
2024-05-09 19:04:54 +0545

    Initial Commit
    Hide/show trello boards

---
+++ content.js
@@ -0,0 +1,81 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === "fetchBoards") {
+        fetchBoards(request.state);
+    } else if (request.action === "showBoard" && request.hasOwnProperty("name")) {
+        showBoard(request.name);
+    } else if (request.action === "hideBoard" && request.hasOwnProperty("name")) {
+        hideBoard(request.name);
+    } else if (request.action === 'showAllBoards') {
+        fetchBoards(request.state);
+    }
+});
+
+function fetchBoards(boardState) {
+    var trelloBoard = document.querySelector("#board");
+    if (trelloBoard) {
+        var boardList = trelloBoard.querySelectorAll("li");
+
+        if (boardList.length) {
+            var boards = [];
+            boardList.forEach(function (li) {
+                var h2Element = li.querySelector("h2");
+                if (h2Element) {
+                    var h2Text = h2Element.innerText;
+                    boards.push(h2Text);
+                }
+            });
+            chrome.runtime.sendMessage({ action: "showBoards", data: boards });
+
+            initBoardsVisibility(boardState, boards)
+        }
+    } else {
+        console.error('Board element with id "#board" not found.');
+    }
+}
+
+function showBoard(boardName) {
+    toggleBoardVisibility(boardName, true);
+}
+
+function hideBoard(boardName) {
+    toggleBoardVisibility(boardName, false);
+}
+
+function initBoardsVisibility(boardState, allBoards) {
+    var boardList = boardState.show;
+
+    if(allBoards.length > 0) {
+        console.log("!boardList.length", !boardList.length)
+        allBoards.forEach(function (board) {
+            toggleBoardVisibility(board, !boardList.length)
+        })
+    }
+
+    if (boardList.length > 0) {
+        console.log("boardList.length", boardList.length)
+        boardList.forEach(function (board) {
+            toggleBoardVisibility(board, true)
+        })
+    }
+}
+
+function toggleBoardVisibility(boardName, display) {
+    var trelloBoard = document.querySelector("#board");
+    if (trelloBoard) {
+        var boardList = trelloBoard.querySelectorAll("li");
+
+        if (boardList.length) {
+            boardList.forEach(function (li) {
+                var h2Element = li.querySelector("h2");
+                if (h2Element) {
+                    var h2Text = h2Element.innerText.toLowerCase().trim();
+                    if (h2Text === boardName.toLowerCase().trim()) {
+                        li.style.display = display ? 'block' : 'none';
+                    }
+                }
+            });
+        }
+    } else {
+        console.error('Board element with id "#board" not found.');
+    }
+}
---
+++ popup.js
@@ -0,0 +1,128 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    var DataStorage = {
+        storage: localStorage,
+        base_key: 'trello_enhanced_',
+        key: function (key) {
+            return this.base_key + key
+        },
+        set: function (key, value) {
+            this.storage.setItem(this.key(key), JSON.stringify(value));
+        },
+        get: function (key) {
+            var item = this.storage.getItem(this.key(key));
+            return JSON.parse(item);
+        },
+        remove: function (key) {
+            this.storage.removeItem(this.key(key));
+        },
+        getBoards: function () {
+            return this.get('boards') || { show: [] }
+        },
+        addBoard: function (board) {
+            var existingBoardData = this.getBoards();
+            existingBoardData.show.push(board);
+            this.set('boards', existingBoardData)
+        },
+        removeBoard: function (board) {
+            var existingBoardData = this.getBoards();
+            var indexToRemove = existingBoardData.show.indexOf(board);
+            if (indexToRemove !== -1) {
+                existingBoardData.show.splice(indexToRemove, 1);
+                this.set('boards', existingBoardData)
+            }
+        }
+    };
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+            action: "fetchBoards",
+            state: DataStorage.getBoards()
+        });
+    });
+
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (request.action === 'showBoards' && request.hasOwnProperty('data')) {
+            createBoardsTable(request.data)
+        }
+    });
+
+    document.getElementById('show-all-boards').addEventListener('click', function() {
+        DataStorage.remove('boards');
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                action: "showAllBoards",
+                state: DataStorage.getBoards()
+            });
+        });
+    });
+
+    function resolveBoardId(board) {
+        return board.toLowerCase().replace(/\s+/g, '_');
+    }
+
+    function createBoardsTable(boards)
+    {
+        var initialState = DataStorage.getBoards();
+
+        var table = document.createElement('table');
+        var thead = document.createElement('thead');
+        var tbody = document.createElement('tbody');
+        var headerRow = document.createElement('tr');
+        var boardHeader = document.createElement('th');
+        var actionsHeader = document.createElement('th');
+        boardHeader.textContent = 'Board';
+        actionsHeader.textContent = 'Actions';
+        headerRow.appendChild(boardHeader);
+        headerRow.appendChild(actionsHeader);
+        thead.appendChild(headerRow);
+        table.appendChild(thead);
+
+        boards.forEach(board => {
+            var row = document.createElement('tr');
+            var boardCell = document.createElement('td');
+            var actionsCell = document.createElement('td');
+
+            var boardId = 'checkbox_' + resolveBoardId(board)
+
+            var label = document.createElement('label');
+            label.textContent = board;
+            label.setAttribute('for', boardId);
+
+            var checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.id = boardId;
+            checkbox.checked = initialState.show.length > 0 ? initialState.show.includes(board) : true;
+            checkbox.setAttribute('data-board', board);
+
+            boardCell.appendChild(label);
+            actionsCell.appendChild(checkbox);
+            row.appendChild(boardCell);
+            row.appendChild(actionsCell);
+            tbody.appendChild(row);
+
+            checkbox.addEventListener('change', toggleBoardVisibility);
+        });
+
+        table.appendChild(tbody);
+        document.getElementById('tableContainer').innerHTML = ''
+        document.getElementById('tableContainer').appendChild(table);
+    }
+
+    function toggleBoardVisibility() {
+        var boardName = this.getAttribute('data-board');
+
+        if (this.checked) {
+            DataStorage.addBoard(boardName);
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { action: "showBoard", name: boardName });
+            });
+        } else {
+            DataStorage.removeBoard(boardName);
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { action: "hideBoard", name: boardName });
+            });
+        }
+    }
+
+});
